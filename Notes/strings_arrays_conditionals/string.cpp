0a5f7757c1e1fec84e3be8b808404e8ffeb5ecfd
Aireos <alex.anderson@ucas-edu.net>
2025-08-25 11:48:18 -0600

    started random nmber notes

--- Notes/strings_arrays_conditionals/string.cpp
+++ Notes/strings_arrays_conditionals/string.cpp
@@ -2,10 +2,14 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
-    int num = rand();
+    // low level verison of random number generator
+    int seconds = time(nullptr);
+    srand(seconds);
+    int num = rand() % 11;
     cout << num;
     return 0;
 }
