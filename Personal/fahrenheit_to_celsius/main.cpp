1261d7d49d06696274d505beb50854d4497a5c4a
Aireos <alex.anderson@ucas-edu.net>
2025-08-25 11:19:05 -0600

    finished Fahrenheit to Celsius converter

---
+++ Personal/fahrenheit_to_celsius/main.cpp
@@ -0,0 +1,20 @@
+// Alexander Anderson, Fahrenheit to Celsius
+
+//importing iostream and making std easyer to use
+#include <iostream>
+using namespace std;
+
+//defining main function
+int main(){
+    //doing two print statments for telling them what the program is and what i need them to import
+    cout << "Fahrenheit to Celsius converter!" << std::endl;
+    cout << "What is the temprature in fahrenheit?: ";
+    //doing the input and defining both needed variables
+    double fahrenheit;
+    cin >> fahrenheit;
+    double celsius;
+    //doing the calculation to turn fahrenheit to celsius
+    celsius = (fahrenheit - 32) * (5.0/9.0);
+    //printing the conversion
+    cout << fahrenheit << " in fahrenheit is equal to " << celsius << " in celsius.";
+}
