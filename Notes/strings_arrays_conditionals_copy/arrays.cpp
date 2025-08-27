023a65fe0795f778ca563cab93843b5020efbefc
Aireos <alex.anderson@ucas-edu.net>
2025-08-27 11:38:41 -0600

    fixed user login and worked on arrays notes

---
+++ Notes/strings_arrays_conditionals_copy/arrays.cpp
@@ -0,0 +1,31 @@
+// Alexander Anderson, Arrays Notes
+
+#include <iostream>
+using namespace std;
+
+string fam[10] = {"Cora","Eric","Alex","Katie","Anderew","Vienna","Tia","Treyson","Xavier","Jake"};
+
+int main(){
+    for(int i=0;size(fam);i++)
+        cout << fam[i] << "LaRose\n";
+    return 0;
+}
+
+// How are for loops constructed in C++
+// 
+// When do you need to use curly brackets in C++
+// 
+// How do you compare items in arrays
+// 
+// How do you use an array as an argument in a function
+// 
+// What is type_t
+// 
+// Why would we use type_t
+// 
+// How do you search an array
+// 
+// How do you sort an array
+// 
+// How do you make a multi-dimensional array
+// 
--- Personal/user_login/main.cpp
+++ Personal/user_login/main.cpp
@@ -6,12 +6,15 @@
 using namespace std;
 
 int main(){
+    //defining the admins and users
     set<string> admins = {"Bob","Joe"};
     set<string> users = {"Billy","Jerald","Max","Alex","Willy"};
     cout << "User login!" << std::endl;
+    // asking for their username
     cout << "What is your username?: ";
     string username;
     cin >> username;
+    // output based on their input
     if (admins.count(username) > 0) {
         cout << "Hello Admin " << username;
     } else if (users.count(username) > 0) {
