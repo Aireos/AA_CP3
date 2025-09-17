18bda263988e5576cc84dadec920a90d3c4a77b9
Aireos <alex.anderson@ucas-edu.net>
2025-09-17 11:42:55 -0600

    Finished user structure

--- Personal/user_structure/main.cpp
+++ Personal/user_structure/main.cpp
@@ -1,4 +1,4 @@
-// AAA User Structure
+// AKA User Structure
 
 #include <iostream>
 #include <string>
@@ -17,14 +17,33 @@ struct User {
     bool admin;
 };
 
+bool operator==(const User& first, const User& second){
+    if(first.name == second.name &&
+        first.password == second.password &&
+        first.admin == second.admin){
+            return true;
+        }else{
+            return false;
+        }
+}
+
+ostream& operator<<(ostream& stream, User& movie){
+    stream << movie.name;
+    return stream;
+}
+
 int main(){
     bool found = false;
     vector<User> users = {{"Jerry", "2", true}, {"Bob", "5", false}, {"Snap", "8", true}, {"Crackle", "8", false}, {"Pop", "8", true}, {"Larose", "8", false}, {"Alex", "8", true}, {"Wakefeild", "8", false}, {"Nick", "8", true}, {"Jonas", "8", false}};
     User newUser;
     cout << "Pick your username: ";
     cin >> newUser.name;
+    cout << "Pick your password: ";
+    cin >> newUser.password;
+    cout << "Are you a admin?(1 for true or 0 for false): ";
+    cin >> newUser.admin;
     for(const auto user: users){
-        if(user.name == newUser.name){
+        if(user == newUser){
             cout << "Welcome back, ";
             if (user.admin) {
                 cout << "Admin ";
@@ -33,11 +52,14 @@ int main(){
             found = true;
         }
     }
-    if(!found){}
-    cout << "Pick your password: ";
-    cin >> newUser.password;
-    cout << "Are you a admin?(t or f): ";
-    cin >> newUser.admin;
+
+    if(!found){
+        cout << "Welcome, ";
+        if (newUser.admin) {
+            cout << "Admin ";
+        }
+        cout << newUser.name << endl;
+    }
 
     return 0;
 }
