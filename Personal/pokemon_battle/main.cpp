957ecf53d6576b9d34f184f17371fec8c7f432ef
Alex <alex.anderson@ucas-edu.net>
2025-09-22 21:11:47 -0600

    finished exploring

--- Personal/pokemon_battle/main.cpp
+++ Personal/pokemon_battle/main.cpp
@@ -1,16 +1,12 @@
-// AKA Pokemon Battle
-
 #include <iostream>
 #include <string>
-#include <limits>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <utility>
-#include <memory>
+#include <algorithm>
 using namespace std;
 
-enum Menu{
+enum Menu {
     Explore = 1,
     Battle = 2,
     Heal_Pokemon = 3,
@@ -31,31 +27,62 @@ struct Pokemen {
     string type;
 };
 
-int main(){
+int main() {
     srand(time(0));
-    vector<Pokemen> Pokedex = {{"Tardigrade", 200, 200, {{100, 1}, {80, 2}, {60, 3}}, 1, "space"},{"Bristlemouth", 120, 120, {{20, 1}, {15, 2}, {10, 3}}, 1, "water"},{"Quelea", 60, 60, {{40, 1}, {30, 2}, {20, 3}}, 1, "sky"},{"Ant", 90, 90, {{30, 1}, {25, 2}, {15, 3}}, 1, "ground"},{"Dodo", 5, 5, {{1, 1}, {1, 2}, {1, 3}}, 1, "useless"}};    
-    cout << endl <<
-    "1. Explore\n" <<
-    "2. Battle\n" <<
-    "3. Heal_Pokemon\n" <<
-    "4. Exit\n" <<
-    "Selection: ";
-    int input;
-    cin >> input;
-    cout << endl;
-    if(input == Menu::Explore){
-        for(int i = 0; i < 5; i++){
-            int randomIndex = rand() % Pokedex.size();
-            Pokemen randomPokemon = Pokedex[randomIndex];
-            cout << "You have found a " << randomPokemon.name << ".";
+    bool all_found = false;
+    vector<Pokemen> Pokedex = {{"Tardigrade", 200, 200, {{100, 1}, {80, 2}, {60, 3}}, 1, "space"}, {"Bristlemouth", 120, 120, {{20, 1}, {15, 2}, {10, 3}}, 1, "water"}, {"Quelea", 60, 60, {{40, 1}, {30, 2}, {20, 3}}, 1, "sky"}, {"Ant", 90, 90, {{30, 1}, {25, 2}, {15, 3}}, 1, "ground"}, {"Dodo", 5, 5, {{1, 1}, {1, 2}, {1, 3}}, 1, "useless"}};;
+    vector<Pokemen> inventory;
+
+    while(true){
+        cout << endl <<
+            "1. Explore\n" <<
+            "2. Battle\n" <<
+            "3. Heal_Pokemon\n" <<
+            "4. Exit\n" <<
+            "Selection: ";
+        int input;
+        cin >> input;
+        cout << endl;
+
+        if (input == Menu::Explore) {
+            if (!all_found){
+                cout << "Exploring for new Pokemon..." << endl;
+                int randomIndex = rand() % Pokedex.size();
+                Pokemen randomPokemon = Pokedex[randomIndex];
+                    
+                bool isDuplicate = false;
+                for (const auto& ownedPokemon : inventory) {
+                    if (ownedPokemon.name == randomPokemon.name) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                    
+                if (!isDuplicate) {
+                    cout << "You have found a " << randomPokemon.name << "! It has been added to your inventory." << endl;
+                    inventory.push_back(randomPokemon);
+
+                }else if(inventory.size() == Pokedex.size()){
+                    cout << "You have found every single pokemon!" << endl;
+                    all_found = true;
+                }else {
+                    cout << "You found a " << randomPokemon.name << ", but you already have one." << endl;
+                }
+            }else{
+                cout << "You have found every single pokemon! Now stop trying to find new ones!" << endl;
+            }
+
+        }else if (input == Menu::Battle) {
+            cout;
+
+        }else if (input == Menu::Heal_Pokemon) {
+            cout;
+
+        }else if (input == Menu::Exit) {
+            return 0;
+
+        }else {
+            cout << "Not an option.\n";
         }
-    }else if(input == Menu::Battle){
-        
-    }else if(input == Menu::Heal_Pokemon){
-        
-    }else if(input == Menu::Exit){
-        return 0;
-    }else{
-        cout << "Not a option.\n";
     }
 }
