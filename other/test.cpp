6b5950135b84b0e1e91f68761013bee969c52d5a
Aireos <alex.anderson@ucas-edu.net>
2025-10-02 11:49:50 -0600

    finished base sorting function

--- other/test.cpp
+++ other/test.cpp
@@ -1,25 +1,48 @@
 #include <iostream>
 #include <string>
-#include <algorithm> // for std::find
-
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <limits>
+#include <fstream>
 using namespace std;
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// function that finds if a string is in a list of strings
 
-string fam[5] = {"Micheal", "Starli", "Alex", "Mara", "Madi"};
-string sibs[3] = {"Alex", "Mara", "Madi"};
 
-// Pass the C-style array as a pointer and its length separately.
-void search(string* list, int len, string item) {
-    // std::find can work with raw pointers and length.
-    // The end iterator is calculated using pointer arithmetic: list + len.
-    if (find(list, list + len, item) != list + len) {
-        cout << "They are my sibling!\n";
-    }
-}
+// string fam[5] = {"Micheal", "Starli", "Alex", "Mara", "Madi"};
+// string sibs[3] = {"Alex", "Mara", "Madi"};
 
-int main() {
-    for (int i = 0; i < size(fam); i++) {
-        cout << fam[i] << " Anderson: ";
-        search(sibs, size(sibs), fam[i]);
-    }
-    return 0;
-}
+// // Pass the C-style array as a pointer and its length separately.
+// void search(string* list, int len, string item) {
+//     // std::find can work with raw pointers and length.
+//     // The end iterator is calculated using pointer arithmetic: list + len.
+//     if (find(list, list + len, item) != list + len) {
+//         cout << "They are my sibling!\n";
+//     }
+// }
+
+// int main() {
+//     for (int i = 0; i < size(fam); i++) {
+//         cout << fam[i] << " Anderson: ";
+//         search(sibs, size(sibs), fam[i]);
+//     }
+//     return 0;
+// }
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//function that sorts a list of intigers
+
+
+// bool compareScores(int a, int b) {
+//     return a > b;
+// };
+
+// int main(){
+//     vector<int> intiger_list = {1,3,2,65,5,4,99};
+//     sort(intiger_list.begin(), intiger_list.end(), compareScores);
+//     for(int i = 0; i < intiger_list.size(); i++){
+//         cout << intiger_list[i] << endl << endl;
+//     }
+// }
