a90f7791fca36f73316ee0b5cdaa4e230be94e9b
Aireos <alex.anderson@ucas-edu.net>
2025-10-10 11:02:06 -0600

    organized everything into folder based on language

---
+++ Other/C++/f_string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+// string f(vector<auto> f_string) {
+//     return f_string.str();
+// }
+
+int main() {
+    string name = "John";
+    int age = 30;
+    
+    // cout << f(static_cast<stringstream>(stringstream() << "Hello, my name is " << name << ", and I am " << age << " years old."));
+}
