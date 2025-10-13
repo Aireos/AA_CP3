956aa6d7f2cd2a929594ba58213a82a4be0f9c64
Aireos <alex.anderson@ucas-edu.net>
2025-10-13 16:45:23 -0600

    Finished Student class

--- Python/Personal/student_class/main.py
+++ Python/Personal/student_class/main.py
@@ -15,12 +15,13 @@ class Student:
     #returns a string with all of the info for 
     def __str__(self):
         return f"ID: {self.id}, Name: {self.name}, Grade: {self.grade}"
-
+#setting orignal student values
 billy=Student(1,"Billy Bob",85)
 snap=Student(2,"Snap Crackle",76)
 pop=Student(3,"Pop Snap",66)
 crackle=Student(4,"Crackle Pop",58)
 bob=Student(5,"Bob Billy",49)
+#printing origanal student info
 print()
 print("Initial student information:")
 print(billy)
@@ -30,9 +31,11 @@ print(crackle)
 print(bob)
 print(Student())
 print()
+#re-setting the grade for 3 students
 bob.set_grade(21)
 billy.set_grade(13)
 snap.set_grade(3)
+#printing the reset grades
 print("Updated student information:")
 print(f"Bob Billy's new grade: {bob.get_grade()}")
 print(f"Billy Bob's new grade: {billy.get_grade()}")
