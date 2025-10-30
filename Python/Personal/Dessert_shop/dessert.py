443c08e8aac513ffc9455e8558e02f8b3ea616ff
Alex <alex.anderson@ucas-edu.net>
2025-10-29 19:55:01 -0600

    finished dessert shop 2

--- Python/Personal/Dessert_shop/dessert.py
+++ Python/Personal/Dessert_shop/dessert.py
@@ -1,69 +1,72 @@
-# AKA Dessert Shop Part 1
+# AKA Dessert Shop Part 2
 
 from abc import ABC, abstractmethod
 
 class DessertItem(ABC):
-    def __init__(self,name = "",tax_percent=7.25):
+    def __init__(self, name="", tax_percent=7.25):
         self.name = name
         self.tax_percent = tax_percent
 
     @abstractmethod
     def calculate_cost(self):
-        return
-    
-    def calculate_tax(self): 
-        return(float(round((((self.tax_percent/100))*self.calculate_cost()))))
+        pass
+
+    def calculate_tax(self):
+        return round(self.calculate_cost() * (self.tax_percent / 100), 2)
+
 
 class Candy(DessertItem):
-    def __init__(self,name = "",price_per_pound=0.0,candy_weight=0.0,tax_percent=7.5):
-        super().__init__(name,tax_percent)
+    def __init__(self, name="", price_per_pound=0.0, candy_weight=0.0, tax_percent=7.25):
+        super().__init__(name, tax_percent)
         self.price_per_pound = price_per_pound
         self.candy_weight = candy_weight
 
     def calculate_cost(self):
-        return(float(self.price_per_pound*self.candy_weight))
+        return round(self.price_per_pound * self.candy_weight, 2)
+
 
 class IceCream(DessertItem):
-    def __init__(self,name = "",price_per_scoop=0.0,scoop_count=0):
-        super().__init__(name)
+    def __init__(self, name="", price_per_scoop=0.0, scoop_count=0, tax_percent=7.25):
+        super().__init__(name, tax_percent)
         self.price_per_scoop = price_per_scoop
         self.scoops = scoop_count
 
     def calculate_cost(self):
-        return(float(self.price_per_scoop*self.scoops))
+        return round(self.price_per_scoop * self.scoops, 2)
+
 
 class Cookie(DessertItem):
-    def __init__(self,name = "",price_per_dozen=0.0,amount=0):
-        super().__init__(name)
+    def __init__(self, name="", price_per_dozen=0.0, amount=0, tax_percent=7.25):
+        super().__init__(name, tax_percent)
         self.price_per_dozen = price_per_dozen
         self.amount = amount
 
     def calculate_cost(self):
-        return(float(round(self.price_per_dozen*(self.amount/12))))
+        return round((self.price_per_dozen * (self.amount / 12)), 2)
+
 
 class Sundae(IceCream):
-    def __init__(self,name = "",price_per_scoop=0.0,scoops=0,topping_name="",topping_price=0.0):
-        super().__init__(name, price_per_scoop, scoops)
+    def __init__(self, name="", price_per_scoop=0.0, scoops=0, topping_name="", topping_price=0.0, tax_percent=7.25):
+        super().__init__(name, price_per_scoop, scoops, tax_percent)
         self.topping_name = topping_name
         self.topping_price = topping_price
-    
-    def calculate_cost(self):
-        return(float((self.price_per_scoop*self.scoops)+(self.topping_price)))
 
-class Order():
+    def calculate_cost(self):
+        return round((self.price_per_scoop * self.scoops) + self.topping_price, 2)
+
+
+class Order:
     def __init__(self):
         self.order = []
-    def add(self,dessert_item):
+
+    def add(self, dessert_item):
         self.order.append(dessert_item)
+
     def __len__(self):
-        return(len(self.order))
+        return len(self.order)
+
     def order_cost(self):
-        total_cost = 0
-        for i in self.order:
-            total_cost += i.calculate_cost()
-        return total_cost
+        return round(sum(item.calculate_cost() for item in self.order), 2)
+
     def order_tax(self):
-        total_tax = 0
-        for i in self.order:
-            total_tax += i.calculate_tax()
-        return total_tax
+        return round(sum(item.calculate_tax() for item in self.order), 2)
--- Python/Personal/Dessert_shop/dessert_shop.py
+++ Python/Personal/Dessert_shop/dessert_shop.py
@@ -1,33 +1,30 @@
-# AKA Dessert Shop Part 1
+# AKA Dessert Shop Part 2
 
 from dessert import *
-import tabulate
+from tabulate import tabulate
 
 def main():
     ordering = Order()
     ordering.add(Candy("Candy Corn", 1.5, 0.25))
-    ordering.add(Candy("Gummy Bears", .25, 0.35))
+    ordering.add(Candy("Gummy Bears", 0.25, 0.35))
     ordering.add(Cookie("Chocolate Chip", 6, 3.99))
-    ordering.add(IceCream("Pistachio", 2, .79))
-    ordering.add(Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29))
+    ordering.add(IceCream("Pistachio", 2, 0.79))
+    ordering.add(Sundae("Vanilla", 3, 0.69, "Hot Fudge", 1.29))
     ordering.add(Cookie("Oatmeal Raisin", 2, 3.45))
-    for item in ordering.order:
-        print(item.name)
-    print(len(ordering))
-    list = [["Candy"],["Cookie"],["IceCream"],["Sundae"]]
-    list[0].append(ordering.order[0])
-    list[0].append(ordering.order[1])
-    list[1].append(ordering.order[2])
-    list[2].append(ordering.order[3])
-    list[3].append(ordering.order[4])
-    list[2].append(ordering.order[5])
-    list_of_lists = []
-    for i in ordering.order:
-        list_of_lists.append([i.name,i.calculate_cost(),i.calculate_tax()])
-    list_of_lists.append(["Subtotal",ordering.order_cost(), ordering.order_tax()])
-    list_of_lists.append(["Total","",ordering.order_cost()*ordering.order_tax()])
-    list_of_lists.append(["Number of items","",ordering.__len__()])
-    print(tabulate.tabulate(list_of_lists, headers=["item","cost","tax"]))
 
-main()
-    
+    list_of_lists = []
+    for item in ordering.order:
+        list_of_lists.append([item.name, f"{item.calculate_cost():.2f}", f"{item.calculate_tax():.2f}"])
+
+    subtotal = ordering.order_cost()
+    total_tax = ordering.order_tax()
+    total = subtotal + total_tax
+    list_of_lists.append(["Subtotal", f"{subtotal:.2f}", f"{total_tax:.2f}"])
+    list_of_lists.append(["Total", f"{total:.2f}", ""])
+    list_of_lists.append(["Number of items", len(ordering), ""])
+
+    print(tabulate(list_of_lists, headers=["Item", "Cost", "Tax"]))
+
+
+if __name__ == "__main__":
+    main()
